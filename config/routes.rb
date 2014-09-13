09e06d52d529f901ac376ebe9e67719fd93b6aa8
rgpass <rgpass@gmail.com>
2014-09-13 11:53:53 -0400

    Add BS Header, Logo, and About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  # StaticPages routes
+  match '/about', to: 'static_pages#about', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
